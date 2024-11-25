0bb26b6fd72adad4f4b91902790fca43671a0410
thabang-m-modiba <thabangmamoloko8@gmail.com>
2024-11-25 21:40:25 +0200

    Update Client side
    *Created a Client.java file

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Java-Client-Server
 Chat bot in Java.
+## Steps
+*Create client.java file
+*Creaye server.java file (With Multi-threading) 
---
+++ src/chatbot/client/Client.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class Client{
+    private static int port = 2024;
+
+    public static void main(String[] args) {
+        try(Socket socket = new Socket("localhost", port);
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            BufferedReader userIn = new BufferedReader(new InputStreamReader(System.in))) {
+
+                boolean running = true;
+                while(running){
+                    System.out.print("Enter: ");
+                    String userMsg = userIn.readLine();
+                    out.println(userMsg);
+                    System.out.println("Server/ Bot: " + in.readLine());
+                    running = false;
+                }
+            
+        } catch (IOException e) {
+            // TODO: handle exception
+            e.printStackTrace();
+        }
+    }
+}
